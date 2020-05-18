24688757ee1640a4553c84149d5d6dc97378c889
Enoch Sousa Filho <enoches@gmail.com>
2020-05-18 02:03:23 -0300

    Inclui scripts - simples testes e balburdia

---
+++ simples-testes.R
@@ -0,0 +1,22 @@
+# Simples testes
+
+# <<<<<<< HEAD
+# Criei um branch "primeiro-branch" e marquei pra não sincronizar com o remote.
+
+# Commitei. Agora farei o segundo commit.
+# Agora.
+# =======
+# 3) Voltei pro master, acrescentei isso. Agora vou comitar e volto lá pro promeiro branch
+# >>>>>>> master
+
+
+library(usethis)
+use_git_credentials()
+github_token()
+edit_r_environ()
+
+# 2020-03-21 
+
+library(renv) 
+
+help("renv")
